ec6bd7353bfedef0180a29b51b7f570ff56f89e1
Randy23123 <rlopez@appdev.yearup.org>
2023-10-23 11:12:06 -0400

    finished home screen

--- src/main/java/com/pluralsight/AccountLedgerApp.java
+++ src/main/java/com/pluralsight/AccountLedgerApp.java
@@ -1,7 +1,37 @@
 package com.pluralsight;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class AccountLedgerApp {
-    public static void main(String[] args) {
-        
+    public static Scanner forPublic = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int homeScreen = 0;
+        while (homeScreen != 4){
+            System.out.println("Home Screen");
+            System.out.println("1.Deposit");
+            System.out.println("2.Payment");
+            System.out.println("3.Ledger");
+            System.out.println("4.Exit");
+            homeScreen = forPublic.nextInt();
+
+            switch(homeScreen){
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    System.out.println("You have exited application :)");
+                    break;
+                default:
+                    System.out.println("\nNot an option pick (1-4)");
+            }
+        }
     }
 }
