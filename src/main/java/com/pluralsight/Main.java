92cf55d846c1af91aacb4d5f6a6ecbd939fa8277
Randy23123 <rlopez@appdev.yearup.org>
2023-10-25 00:13:02 -0400

    forgot to add main

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Reader.reader();
+        AccountLedgerApp.AccountLedgerApp();
+    }
+}
